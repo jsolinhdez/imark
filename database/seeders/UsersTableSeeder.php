5156c59ffa95682e4b9747ace959075c015d1670
jsolinhdez <jsolinhdez@gmail.com>
2022-07-08 23:06:45 -0400

    Initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+//            Admin
+            [
+                'full_name' => 'Solin Admin',
+                'username' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('1111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+//            Vendor
+            [
+                'full_name' => 'Solin Vendor',
+                'username' => 'Vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => bcrypt('1111'),
+                'role' => 'vendor',
+                'status' => 'active',
+            ],
+//            Customer
+            [
+                'full_name' => 'Solin Customer',
+                'username' => 'Customer',
+                'email' => 'customer@gmail.com',
+                'password' => bcrypt('1111'),
+                'role' => 'customer',
+                'status' => 'active',
+            ]
+        ]);
+    }
+}

1a1840b0578e77e61b250d1eecaa1afc1a3e9e53
jsolinhdez <jsolinhdez@gmail.com>
2022-08-02 02:18:58 -0400

    a simple responsive issue

--- resources/views/frontend/pages/checkout/checkout4.blade.php
+++ resources/views/frontend/pages/checkout/checkout4.blade.php
@@ -31,14 +31,16 @@
                             <td class="align-middle"><img src="{{ $item->model->photo }}" alt=""
                                                           style="width: 50px;">
                             </td>
-                            <td class="align-middle"><a href="{{ route('product.detail', $item->model->slug) }}">{{ $item->name }}</a>
+                            <td class="align-middle"><a
+                                    href="{{ route('product.detail', $item->model->slug) }}">{{ $item->name }}</a>
                             </td>
                             <td class="align-middle">$ {{ number_format($item->price,2) }}</td>
                             <td class="align-middle" style="width: 20px">
                                 <div class="input-group-btn quantity" id="cart_counter">
                                     <input type="number" class="qty-text" data-id="{{ $item->rowId }}"
                                            id="qty-input-{{ $item->rowId }}"
-                                           name="quantity" value="{{ $item->qty }}" disabled style="border: none;width: 60%">
+                                           name="quantity" value="{{ $item->qty }}" disabled
+                                           style="border: none;width: 60%">
 
                                 </div>
                             </td>
@@ -48,54 +50,62 @@
                     @endforeach
                     </tbody>
                 </table>
-                <div class="row p-5">
-                <div class="card border-secondary col-md-4 mt-2 p-5" id="data_total">
-                    <div class="card-header bg-secondary border-0">
-                        <h4 class="font-weight-semi-bold m-0">Cart Totals</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between mb-3 pt-1">
-                            <h6 class="font-weight-medium">Subtotal</h6>
-                            <h6 class="font-weight-medium">$ {{ Cart::instance('shopping')->subtotal() }}</h6>
+                <div class="row ">
+                    <div class="card border-secondary col-md-12 mt-2 p-5" id="data_total">
+                        <div class="card-header bg-secondary border-0">
+                            <h4 class="font-weight-semi-bold m-0">Cart Totals</h4>
                         </div>
-                        <div class="d-flex justify-content-between mb-3 pt-1">
-                            <h6 class="font-weight-medium">Shipping</h6>
-                            <h6 class="font-weight-medium">+ $ {{ number_format(\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'],2) }}</h6>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between mb-3 pt-1">
+                                <h6 class="font-weight-medium">Subtotal</h6>
+                                <h6 class="font-weight-medium">$ {{ Cart::instance('shopping')->subtotal() }}</h6>
+                            </div>
+                            <div class="d-flex justify-content-between mb-3 pt-1">
+                                <h6 class="font-weight-medium">Shipping</h6>
+                                <h6 class="font-weight-medium">+
+                                    $ {{ number_format(\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'],2) }}</h6>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <h6 class="font-weight-medium">Coupon</h6>
+                                @if(session()->has('coupon'))
+                                    <h6 class="price">-
+                                        $ {{ number_format(\Illuminate\Support\Facades\Session::get('coupon')['value'],2) }}</h6>
+                                @else
+                                    <h6 class="price">- $ {{ number_format(0,2) }}</h6>
+                                @endif
+                            </div>
                         </div>
-                        <div class="d-flex justify-content-between">
-                            <h6 class="font-weight-medium">Coupon</h6>
-                            @if(session()->has('coupon'))
-                                <h6 class="price">- $ {{ number_format(\Illuminate\Support\Facades\Session::get('coupon')['value'],2) }}</h6>
-                            @else
-                                <h6 class="price">- $ {{ number_format(0,2) }}</h6>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="card-footer border-secondary bg-transparent">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5 class="font-weight-bold">Total</h5>
-                            @if(\Illuminate\Support\Facades\Session::has('coupon') && \Illuminate\Support\Facades\Session::has('checkout'))
-                                <h5 class="font-weight-bold">$ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())+\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'] - \Illuminate\Support\Facades\Session::get('coupon')['value'],2) }} </h5>
-                            @elseif(\Illuminate\Support\Facades\Session::has('coupon'))
-                                <h5 class="font-weight-bold">$ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())-\Illuminate\Support\Facades\Session::get('coupon')['value'],2) }} </h5>
-                            @elseif(\Illuminate\Support\Facades\Session::has('checkout'))
-                                <h5 class="font-weight-bold">$ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())+\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'],2) }} </h5>
-                            @else
-                                <h5 class="font-weight-bold">$ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal()),2) }} </h5>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2 mt-2 p-5" ></div>
-                <div class=" col-md-4 mt-2 p-5" >
-                    <div class="">
-                        <div class="mt-4" >
-                            <a class="btn btn-secondary w-100 border-0 py-3" href="{{ route('checkout1') }}">Go Back</a>
-                            <a href="{{ route('checkout.store') }}" class="btn w-100 btn-success border-0 mt-4 py-3">Confirm</a>
+                        <div class="card-footer border-secondary bg-transparent">
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-bold">Total</h5>
+                                @if(\Illuminate\Support\Facades\Session::has('coupon') && \Illuminate\Support\Facades\Session::has('checkout'))
+                                    <h5 class="font-weight-bold">
+                                        $ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())+\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'] - \Illuminate\Support\Facades\Session::get('coupon')['value'],2) }} </h5>
+                                @elseif(\Illuminate\Support\Facades\Session::has('coupon'))
+                                    <h5 class="font-weight-bold">
+                                        $ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())-\Illuminate\Support\Facades\Session::get('coupon')['value'],2) }} </h5>
+                                @elseif(\Illuminate\Support\Facades\Session::has('checkout'))
+                                    <h5 class="font-weight-bold">
+                                        $ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal())+\Illuminate\Support\Facades\Session::get('checkout')[0]['delivery_charge'],2) }} </h5>
+                                @else
+                                    <h5 class="font-weight-bold">
+                                        $ {{ number_format((float)str_replace(',','',\Gloudemans\Shoppingcart\Facades\Cart::instance('shopping')->subtotal()),2) }} </h5>
+                                @endif
+                            </div>
                         </div>
                     </div>
 
-                </div>
+                    <div class=" col-md-4 mt-2 p-5">
+                        <div class="">
+                            <div class="mt-4">
+                                <a class="btn btn-secondary w-100 border-0 py-3" href="{{ route('checkout1') }}">Go
+                                    Back</a>
+                                <a href="{{ route('checkout.store') }}"
+                                   class="btn w-100 btn-success border-0 mt-4 py-3">Confirm</a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
             </form>
